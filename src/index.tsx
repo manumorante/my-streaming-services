759144c2274aaca349d38c94d6711bf94f19c1a3
Manu <manu@estadologico.com>
2023-04-28 10:02:05 +0200

    skyshowtime justwatch

--- src/index.tsx
+++ src/index.tsx
@@ -1,72 +1,15 @@
-import { ActionPanel, Action, Icon, List } from "@raycast/api";
-
-const ITEMS = [
-  {
-    id: 1,
-    title: "Netflix",
-    icon: "netflix.ico",
-    subtitle: "",
-    accessory: "",
-    accessoryIcon: Icon.CheckCircle,
-    url: "https://www.netflix.com",
-  },
-  {
-    id: 2,
-    title: "Disney+",
-    icon: "disneyplus.ico",
-    subtitle: "",
-    accessory: "",
-    accessoryIcon: Icon.CheckCircle,
-    url: "https://www.disneyplus.com/es-es/home",
-  },
-  {
-    id: 3,
-    title: "HBO",
-    icon: "hbo.ico",
-    subtitle: "",
-    accessory: "",
-    accessoryIcon: Icon.CheckCircle,
-    url: "https://play.hbomax.com/page/urn:hbo:page:home",
-  },
-  {
-    id: 4,
-    title: "Prime Video",
-    icon: "primevideo.ico",
-    subtitle: "",
-    accessory: "",
-    accessoryIcon: Icon.CheckCircle,
-    url: "https://www.primevideo.com/",
-  },
-  {
-    id: 5,
-    title: "Apple TV+",
-    icon: "appletv.ico",
-    subtitle: "",
-    accessory: "",
-    accessoryIcon: Icon.CheckCircle,
-    url: "https://tv.apple.com/es",
-  },
-  {
-    id: 6,
-    title: "Filmin",
-    icon: "filmin.webp",
-    subtitle: "",
-    accessory: "",
-    accessoryIcon: Icon.Circle,
-    url: "https://www.filmin.es",
-  },
-];
+import { SERVICES } from '../data'
+import { ActionPanel, Action, List } from '@raycast/api'
 
 export default function Command() {
   return (
     <List>
-      {ITEMS.map((item) => (
+      {SERVICES.map((item) => (
         <List.Item
           key={item.id}
           icon={item.icon}
           title={item.title}
           subtitle={item.subtitle}
-          accessories={[{ icon: item.accessoryIcon, text: item.accessory }]}
           actions={
             <ActionPanel>
               <Action.OpenInBrowser url={item.url} />
@@ -75,5 +18,5 @@ export default function Command() {
         />
       ))}
     </List>
-  );
+  )
 }
