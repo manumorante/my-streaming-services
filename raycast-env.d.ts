2a5752bdc0bc7f0e24fa4afced24826119f099d2
Manu Morante <manu@estadologico.com>
2024-06-09 07:19:53 +0200

    big update

--- raycast-env.d.ts
+++ raycast-env.d.ts
@@ -7,19 +7,24 @@
 
 /* eslint-disable @typescript-eslint/ban-types */
 
-type ExtensionPreferences = {}
+type ExtensionPreferences = {
+  /** Use Google Chrome - Use Google Chrome */
+  "UseGoogleChrome": "yes" | "no",
+  /** Google Chrome Profile - Google Chrome Profile */
+  "GoogleProfile": string
+}
 
 /** Preferences accessible in all the extension's commands */
 declare type Preferences = ExtensionPreferences
 
 declare namespace Preferences {
-  /** Preferences accessible in the `index` command */
-  export type Index = ExtensionPreferences & {}
+  /** Preferences accessible in the `search-services` command */
+  export type SearchServices = ExtensionPreferences & {}
 }
 
 declare namespace Arguments {
-  /** Arguments passed to the `index` command */
-  export type Index = {}
+  /** Arguments passed to the `search-services` command */
+  export type SearchServices = {}
 }
 
 
